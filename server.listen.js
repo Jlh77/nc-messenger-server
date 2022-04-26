1765af663e9f99dc78037506a62a9c289c4410d5
Joe Hawkins <josephlhawkins2@gmail.com>
2022-04-26 12:14:14 +0100

    refine layout and fix routes bug

--- server.listen.js
+++ server.listen.js
@@ -1,9 +1,52 @@
 const app = require("./server");
-const { PORT = 9090 } = process.env;
+const { PORT = 8080 } = process.env;
 
 const server = app.listen(PORT, (err) => {
   if (err) throw err;
   console.log(`Running chat server at ${PORT}...`);
 });
 
-module.exports = server;
+const io = require("socket.io")(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"],
+  },
+});
+
+const defaultUser = {
+  id: "anon",
+  name: "Anonymous",
+};
+const messages = new Set();
+
+// const apiRouter = require("./routers/api.router");
+// app.use("/api", apiRouter);
+
+// Messaging
+io.on("connection", (socket) => {
+  console.log("User has entered the chat");
+  socket.on("disconnect", () => {
+    console.log("User has left the chat");
+  });
+
+  socket.on("getMessages", () => {
+    messages.forEach((message) => {
+      socket.emit("message", message);
+    });
+  });
+
+  socket.on("message", (value) => {
+    console.log("someone sent a message!");
+
+    const message = {
+      id: 20,
+      user: defaultUser,
+      value,
+      time: Date.now(),
+    };
+
+    messages.add(message);
+
+    socket.emit("message", message);
+  });
+});
