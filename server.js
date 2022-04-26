f26d0b23d86e167c50310f340dbffe012ec8bc92
Joe Hawkins <josephlhawkins2@gmail.com>
2022-04-26 12:27:10 +0100

    heroku expres test

--- server.js
+++ server.js
@@ -6,11 +6,11 @@ app.use(cors());
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.send("test");
+  res.send("<h1>Hello</h1");
 });
 
 app.get("/test", (req, res) => {
-  res.send("it works");
+  res.send("<h1>test</h1");
 });
 
 module.exports = app;
--- server.listen.js
+++ server.listen.js
@@ -6,47 +6,44 @@ const server = app.listen(PORT, (err) => {
   console.log(`Running chat server at ${PORT}...`);
 });
 
-// const io = require("socket.io")(server, {
-//   cors: {
-//     origin: "*",
-//     methods: ["GET", "POST"],
-//   },
-// });
+const io = require("socket.io")(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"],
+  },
+});
 
-// const defaultUser = {
-//   id: "anon",
-//   name: "Anonymous",
-// };
-// const messages = new Set();
+const defaultUser = {
+  id: "anon",
+  name: "Anonymous",
+};
+const messages = new Set();
 
-// // const apiRouter = require("./routers/api.router");
-// // app.use("/api", apiRouter);
+// Messaging
+io.on("connection", (socket) => {
+  console.log("User has entered the chat");
+  socket.on("disconnect", () => {
+    console.log("User has left the chat");
+  });
 
-// // Messaging
-// io.on("connection", (socket) => {
-//   console.log("User has entered the chat");
-//   socket.on("disconnect", () => {
-//     console.log("User has left the chat");
-//   });
+  socket.on("getMessages", () => {
+    messages.forEach((message) => {
+      socket.emit("message", message);
+    });
+  });
 
-//   socket.on("getMessages", () => {
-//     messages.forEach((message) => {
-//       socket.emit("message", message);
-//     });
-//   });
+  socket.on("message", (value) => {
+    console.log("someone sent a message!");
 
-//   socket.on("message", (value) => {
-//     console.log("someone sent a message!");
+    const message = {
+      id: 20,
+      user: defaultUser,
+      value,
+      time: Date.now(),
+    };
 
-//     const message = {
-//       id: 20,
-//       user: defaultUser,
-//       value,
-//       time: Date.now(),
-//     };
+    messages.add(message);
 
-//     messages.add(message);
-
-//     socket.emit("message", message);
-//   });
-// });
+    socket.emit("message", message);
+  });
+});
